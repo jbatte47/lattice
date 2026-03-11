531bd7974b56470f40f8ff92db884efdef85c386
John Batte <jbatte@gmail.com>
2026-03-10 23:28:09 -0500

    feat(components): add card component Fixes #17 (#83)

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { Button, type ButtonProps } from './Button';
 export { Badge, type BadgeProps } from './Badge';
 export { Text, type TextProps } from './Text';
+export { Card, type CardProps } from './Card';
