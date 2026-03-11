4c7c810bf5138ec2541a066ae8dc3115c51eb4a3
jbatte47 <jbatte@gmail.com>
2026-03-10 22:39:06 -0500

    feat(components): add button component Fixes #13

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts', 'src/tailwind/preset.ts'],
+  entry: ['src/index.ts', 'src/components/index.ts', 'src/tailwind/preset.ts'],
   format: ['cjs', 'esm'],
   dts: true,
   sourcemap: true,
   clean: true,
   target: 'es2020',
+  external: ['react', 'react-dom'],
 });
